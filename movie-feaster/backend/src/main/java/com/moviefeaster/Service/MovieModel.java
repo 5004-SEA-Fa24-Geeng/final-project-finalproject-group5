8890cec032c5bb70858f7efb47a5d15a1cbb5355
happy1claire <happy1claire@gmail.com>
2025-04-12 14:07:03 -0700

    restore model file to comply new form

--- movie-feaster/backend/src/main/java/com/moviefeaster/Service/MovieModel.java
+++ movie-feaster/backend/src/main/java/com/moviefeaster/Service/MovieModel.java
@@ -1,47 +1,167 @@
-package com.moviefeaster.Service;
+package Service;
 
-import com.moviefeaster.Model.*;
-import com.moviefeaster.Model.Movie;
-import org.springframework.stereotype.Service;
+import Model.*;
+import Utils.DataFormatter;
+import Utils.MovieFilterFacilitator;
+import Utils.MovieSorter;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-@Service
 public class MovieModel implements MovieModelInterface {
 
+    /** Singleton instance */
+    private static MovieModel instance;
     /** Movie's list */
     private List<Movie> movies;
+    /** Filtered movies' list */
+    private List<Movie> processedMovies;
 
     /**
-     * Public constructor for Java bean convention
+     * Private constructor to prevent direct instantiation
      */
-    public MovieModel() {
-        this.movies = new ArrayList<>();
-        fetchMovies(); // Optionally auto-fetch at startup
+    private MovieModel() {
     }
 
+    /** Fetch movie's data from TMDB api and ingest into movie's field.
+     * Set the processed movies to an empty list.
+     */
     @Override
     public void fetchMovies() {
         this.movies = MovieParser.getMoviesFromApi();
+        this.processedMovies = new ArrayList<>();
     }
 
+    /** Get the original movies that pull from TMDB api. */
     public List<Movie> getMovies() {
         return this.movies;
     }
 
-    @Override
-    public void writeFile(Format format) {
-        System.out.println(format);
-        return;
+    /** Get the processed movies that applied sorter and filter. */
+    public List<Movie> getProcessedMoviesMovies() {
+        return this.processedMovies;
     }
 
+    /** Write movie's record to the local file with an option to use original data from TMDB api or processed data.
+     * @param format the format of the file
+     * @param useProcessedMovie if true, use processed data, otherwise, use original data.
+     */
     @Override
-    public List<Movie> searchByMultipleFilters(String title, String director, String parsedCast, int year, Genre genre) {
-        // Testing
-        return getMovies();
+    public void writeFile(boolean useProcessedMovie, Format format) {
+        List<Movie> moviesToWrite = useProcessedMovie ? this.processedMovies : this.movies;
+
+        try (OutputStream out = new FileOutputStream("output." + format.toString().toLowerCase())) {
+            DataFormatter.write(moviesToWrite, format, out);
+            System.out.println("Movie data written successfully in " + format + " format.");
+        } catch (IOException e) {
+            System.err.println("Failed to write movie data: " + e.getMessage());
+        }
     }
 
+    /**
+     * Filtered the movie list by filter strategy.
+     * @param filtersStrategy filtered strategies that with filter type and corresponding values.
+     * @return Filtered movies list.
+     */
+    @Override
+    public List<Movie> searchByMultipleFilters(Map<MovieFilterType, Object> filtersStrategy) {
+        if (filtersStrategy == null) {
+            return this.movies;
+        }
+
+        List<Movie> filteredMovies = MovieFilterFacilitator.filter(this.movies, filtersStrategy);
+        return filteredMovies;
+    }
+
+    /**
+     * Filtered the movie list by filter strategy with an option to use a movie list that is filtered or the original
+     * data that pull from TMDB api.
+     * @param newFilter whether to use a movie list that is filtered or the original data that pull from TMDB api.
+     * @param filtersStrategy  filtersStrategy filtered strategies that with filter type and corresponding values.
+     * @return Filtered movies list.
+     */
+    public List<Movie> searchByMultipleFilters(boolean newFilter, Map<MovieFilterType, Object> filtersStrategy) {
+        if (filtersStrategy == null) {
+            return this.movies;
+        }
+
+        List<Movie> moviesToFilter;
+
+        if (newFilter) {
+            moviesToFilter = this.movies;
+        } else {
+            moviesToFilter = this.processedMovies;
+        }
+
+        return MovieFilterFacilitator.filter(moviesToFilter, filtersStrategy);
+    }
+
+    /**
+     * Sorts the movie list by sorting strategy.
+     *
+     * @param sortType     the sorting strategy to be applied to the movie list
+     * @return a new list of movies sorted according to the given strategy
+     */
+    public List<Movie> sortMovieList(MovieSortType sortType) {
+        // If no sort type, return original data.
+        if (sortType == null) {
+            return this.movies;
+        }
+
+        List<Movie> moviesToSort = this.movies;
+        List<Movie> sortedMovies = switch (sortType) {
+            case TITLE_ASC -> MovieSorter.sortByTitle(moviesToSort);
+            case TITLE_DESC -> MovieSorter.sortByTitleDescending(moviesToSort);
+            case YEAR_ASC -> MovieSorter.sortByYearAscending(moviesToSort);
+            case YEAR_DESC -> MovieSorter.sortByYear(moviesToSort);
+            case RATING_ASC -> MovieSorter.sortByRatingAscending(moviesToSort);
+            case RATING_DESC -> MovieSorter.sortByRating(moviesToSort);
+            case INAPP_RATING_ASC -> MovieSorter.sortByInAppRatingAscending(moviesToSort);
+            case INAPP_RATING_DESC -> MovieSorter.sortByInAppRating(moviesToSort);
+        };
+
+        this.processedMovies = sortedMovies;
+        return sortedMovies;
+    }
+
+    /**
+     * Sorts the movie list by sorting strategy with an option to use original data or processed data.
+     *
+     * @param sortType     the sorting strategy to be applied to the movie list
+     * @param useFiltered  if true, sort the filtered (processed) list; otherwise, sort the original movie list
+     * @return a movie list that applied the sorting strategy.
+     */
+    public List<Movie> sortMovieList(boolean useFiltered, MovieSortType sortType) {
+        // If no sort type, return original data.
+        if (sortType == null) {
+            return this.movies;
+        }
+
+        List<Movie> moviesToSort = useFiltered ? this.processedMovies : this.movies;
+
+        List<Movie> sortedMovies = switch (sortType) {
+            case TITLE_ASC -> MovieSorter.sortByTitle(moviesToSort);
+            case TITLE_DESC -> MovieSorter.sortByTitleDescending(moviesToSort);
+            case YEAR_ASC -> MovieSorter.sortByYearAscending(moviesToSort);
+            case YEAR_DESC -> MovieSorter.sortByYear(moviesToSort);
+            case RATING_ASC -> MovieSorter.sortByRatingAscending(moviesToSort);
+            case RATING_DESC -> MovieSorter.sortByRating(moviesToSort);
+            case INAPP_RATING_ASC -> MovieSorter.sortByInAppRatingAscending(moviesToSort);
+            case INAPP_RATING_DESC -> MovieSorter.sortByInAppRating(moviesToSort);
+        };
+
+        this.processedMovies = sortedMovies;
+        return sortedMovies;
+    }
+
+    /** Update the inApp comment
+     * @param movieID movie's ID
+     * @param comment user's comment
+     */
     @Override
     public void UpdateComments(int movieID, String comment) {
         for (Movie movie : this.movies) {
@@ -52,6 +172,10 @@ public class MovieModel implements MovieModelInterface {
         }
     }
 
+    /** Update the inApp rating
+     * @param movieID movie's ID
+     * @param rating user's rating
+     */
     @Override
     public void UpdateRating(int movieID, double rating) {
         for (Movie movie : this.movies) {
@@ -62,4 +186,16 @@ public class MovieModel implements MovieModelInterface {
         }
     }
 
+    /**
+     * Gets the singleton instance of the MovieModel.
+     * It prevents model being instantiated more than once.
+     * @return MovieModel
+     */
+    public static MovieModel getInstance() {
+        if (instance == null) {
+            instance = new MovieModel();
+            instance.fetchMovies();
+        }
+        return instance;
+    }
 }
